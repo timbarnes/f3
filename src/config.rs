39b284343ffce6a3a801e3830b1a79f525ace848
Tim Barnes <tw@timandmarina.com>
2025-06-11 10:51:17 -0700

    Refactored main and config to separate engine from configuration

--- src/config.rs
+++ src/config.rs
@@ -1,27 +1,16 @@
 // system configuration and command line processing
 
-use crate::engine::TF;
 use crate::messages::DebugLevel;
-
 use ::clap::{arg, Command};
 
-const VERSION: &str = "alpha.24.3.18";
-const WELCOME_MESSAGE: &str = "Welcome to f2.";
-const EXIT_MESSAGE: &str = "Finished";
-const DEFAULT_CORE: [&str; 3] = ["./corelib.fs", "~/.f2/corelib.fs", "src/forth/corelib.fs"];
-
-macro_rules! push {
-    ($self:ident, $val:expr) => {
-        $self.stack_ptr -= 1;
-        $self.data[$self.stack_ptr] = $val;
-    };
-}
+pub const VERSION: &str = "alpha.25.6.11";
+pub const DEFAULT_CORE: [&str; 3] = ["./corelib.fs", "~/.f2/corelib.fs", "src/forth/corelib.fs"];
 
 pub struct Config {
-    debug_level: DebugLevel,
-    loaded_file: String,
-    core_file: String,
-    no_core: bool,
+    pub debug_level: DebugLevel,
+    pub loaded_file: String,
+    pub core_file: String,
+    pub no_core: bool,
     pub run: bool,
 }
 
@@ -42,7 +31,7 @@ impl Config {
         // process arguments
         // let msg = Msg::new(); // Create a message handler for argument errors
 
-        let arguments = Command::new("tForth")
+        let arguments = Command::new("f3")
             .version(VERSION)
             .author("Tim Barnes")
             .about("A simple Forth interpreter")
@@ -81,43 +70,5 @@ impl Config {
             self.loaded_file = file.clone();
         }
         self
-    }
-
-    /// run_forth is the main entry point that performs the cold start operations, loads library files,
-    ///     and hands off control to the main interpreter loop
-    ///
-    pub fn run_forth(&mut self) {
-        // create and run the interpreter
-        // return when finished
-        fn load_file(interpreter: &mut TF, file_name: &str) {
-            TF::u_set_string(
-                interpreter,
-                interpreter.data[interpreter.tmp_ptr] as usize,
-                file_name,
-            );
-            push!(interpreter, interpreter.data[interpreter.tmp_ptr]);
-            interpreter.f_include_file();
-        }
-
-        let mut forth = TF::new();
-        forth.cold_start();
-        if !self.no_core {
-            for path in DEFAULT_CORE {
-                load_file(&mut forth, &path);
-            }
-        }
-        if self.loaded_file != "" {
-            load_file(&mut forth, &self.loaded_file);
-        }
-
-        forth.set_abort_flag(false); // abort flag may have been set by load_file, but is no longer needed.
-
-        println!("{WELCOME_MESSAGE} Version {VERSION}");
-
-        // Enter the interactive loop to read and process input
-        // call QUERY to start the r2 engine.
-        forth.f_quit();
-        // Exit when query gets a bye or EOF.
-        println!("{EXIT_MESSAGE}");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,59 @@ mod config;
 mod engine;
 mod messages;
 mod files;
-//mod tokenizer;
 mod internals;
 
-use config::Config;
+use config::{Config, DEFAULT_CORE, VERSION};
+
+const WELCOME_MESSAGE: &str = "Welcome to f3.";
+const EXIT_MESSAGE: &str = "Finished";
+
+macro_rules! push {
+    ($self:ident, $val:expr) => {
+        $self.stack_ptr -= 1;
+        $self.data[$self.stack_ptr] = $val;
+    };
+}
+
+fn run_forth(config: &Config) {
+    use engine::TF;
+    fn load_file(interpreter: &mut TF, file_name: &str) {
+        TF::u_set_string(
+            interpreter,
+            interpreter.data[interpreter.tmp_ptr] as usize,
+            file_name,
+        );
+        push!(interpreter, interpreter.data[interpreter.tmp_ptr]);
+        interpreter.f_include_file();
+    }
+
+    let mut forth = TF::new();
+    forth.cold_start();
+    if !config.no_core {
+        for path in DEFAULT_CORE {
+            load_file(&mut forth, &path);
+        }
+    }
+    if config.loaded_file != "" {
+        load_file(&mut forth, &config.loaded_file);
+    }
+
+    forth.set_abort_flag(false); // abort flag may have been set by load_file, but is no longer needed.
+
+    println!("{WELCOME_MESSAGE} Version {VERSION}");
+
+    // Enter the interactive loop to read and process input
+    // call QUERY to start the r2 engine.
+    forth.f_quit();
+    // Exit when query gets a bye or EOF.
+    println!("{EXIT_MESSAGE}");
+}
 
 fn main() {
     let mut config = Config::new();
     config.process_args();
 
     if config.run {
-        config.run_forth();
+        run_forth(&config);
     }
 }
