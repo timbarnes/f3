cdde0dcf0dcad42dd92fc0b8074412960bb290c1
Tim Barnes <tw@timandmarina.com>
2025-07-06 21:13:25 -0700

    Added string-dump capabilities

--- src/forth/debug.fs
+++ src/forth/debug.fs
@@ -19,7 +19,57 @@ variable word-counter
                         drop cr ;
 
 : print-word ( xt -- )
-                    1- @ 13 ltype ;                 \ print a word name, given the xt as produced by '
+                    1- @ 13 ltype ;                             \ print a word name, given the xt as produced by '
+
+\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\ String Dump capability
+\ dump-stringspace - dumps buffers and strings
+\ dump-strings ( s_addr -- ) - dumps multiple strings from s_addr (must be a count byte)
+\ dump-string  ( s_addr -- ) - dumps a single string at s_addr
+
+132 constant buf-len
+
+\ Dump a string buffer from the beginning, for <count> characters
+: dump-buffer ( s_addr -- )    \ Dump contents of a string input buffer
+    ." Buffer at " dup 0 .r ." , length " dup c@ 0 .r ." : "
+    buf-len incr-for
+    for dup i - c@ emit next
+    drop cr
+    26 spaces
+    c@ for
+        '-' emit
+    next cr
+    ;
+
+\ Move the address to the next counted string
+: next-string ( s_addr -- s_addr )
+    c@ + 1+
+    ;
+
+\ Dump a counted string, given a starting address
+: dump-string ( s_addr -- )
+    dup 5 .r ." /" dup c@ 3 .r ." : "
+    dup type cr drop ;
+
+\ Dump strings from a starting point, stopping when there are no more (count is zero)
+: dump-strings ( s_addr -- )
+    begin
+        s-here @ over < if drop exit then           \ We've run out of strings
+        dup dump-string
+        dup next-string
+    again
+    ;
+
+\ Print all string space
+: dump-stringspace ( -- )
+    ." TIB: " cr
+    0 dump-buffer    \ TIB
+    ." PAD " cr
+    pad @ dump-buffer
+    ." TMP " cr
+    tmp @ dump-buffer
+    tmp @ 132 + dump-strings
+    ;
 
 
 \ Memory Dump Utility
--- src/forth/extras.fs
+++ src/forth/extras.fs
@@ -1,37 +1,44 @@
 \ Extra definitions, code under development
 
-\ Memory Dump capability
-\ dump-help, dump, dump-stringspace (under development)
+\ String Dump capability
+\ dump-stringspace
 \
-
 
 132 constant buf-len
 
+\ Dump a string buffer from the beginning, for <count> characters
 : dump-buffer ( s_addr -- )    \ Dump contents of a string input buffer
+    ." Buffer at " dup 0 .r ." , length " dup c@ 0 .r ." : "
     buf-len incr-for
     for dup i - c@ emit next
-    drop drop cr
+    drop cr
+    26 spaces
+    c@ for
+        '-' emit
+    next cr
     ;
 
-: dump-strings ( s_addr -- )    \ Dump the counted strings with their addresses
-    dup                         ( s_addr s_addr )
+\ Move the address to the next counted string
+: next-string ( s_addr -- s_addr )
+    c@ + 1+
+    ;
+
+\ Dump a counted string, given a starting address
+: dump-string ( s_addr -- )
+    dup 5 .r ." /" dup c@ . ." : "
+    dup type cr drop ;
+
+\ Dump strings from a starting point, stopping when there are no more (count is zero)
+: dump-strings ( s_addr -- )
     begin
-        c@                      ( s_addr c )
-        dup                     ( s_addr c c )
-        0=                      ( s_addr c bool )
-        if                      ( s_addr c )
-            ." Done " cr        ( s_addr c )
-            drop                ( s_addr )
-            exit                
-        else
-            2dup + 2 roll 
-            type space      ( s_addr c)
-            +                   ( s_addr c+s_addr )
-        then
+        s-here @ over < if drop exit then           \ We've run out of strings
+        dup dump-string
+        dup next-string
     again
     ;
 
-: dump-stringspace ( s_addr -- s_addr ).  \ dump n cells from string space
+\ Print all string space
+: dump-stringspace ( -- )
     ." TIB: " cr
     0 dump-buffer    \ TIB
     ." PAD " cr
@@ -40,4 +47,3 @@
     tmp @ dump-buffer
     tmp @ 132 + dump-strings
     ;
-
